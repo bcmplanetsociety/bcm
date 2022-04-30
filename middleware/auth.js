0626e01de495b56457693d752d182d8506964582
saieesh <saieesh.p.h@doodleblue.com>
2022-04-30 17:21:30 +0530

    Added date picker in even view.

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,7 @@
 
 //For SuperAdmin user
 const superAdminAuthenticated = (req, res, next) => {
+    if(req.user){
     if(req.user.role === 0){
         return next();
     } else {
@@ -8,15 +9,18 @@ const superAdminAuthenticated = (req, res, next) => {
         res.redirect('/login');
     }
 }
+}
 
 //For Admin user
 const adminAuthenticated = (req, res, next) => {
+    if(req.user){
     if(req.user.role === 0 || req.user.role === 1){
         return next();
     } else {
         //req.flash('error_msg','You are not logged in');
         res.redirect('/login');
     }
+   }
 }
 
 //For Normal user
